4cef16d0fc2b06a225ce9923d53ff1667b9bf1d2
Jos33y <josephlagbalu@gmail.com>
2023-02-28 18:30:19 -0600

    bootstrap navbar corrected

--- client/src/store/components/header.js
+++ client/src/store/components/header.js
@@ -3,9 +3,7 @@ import "./component.css";
 import { Link, useNavigate } from "react-router-dom";
 import TimaBlaq from "../assets/images/timablaq.jpeg";
 
-
 const HeaderNav = () => {
-
     const navigate = useNavigate();
 
     return (
@@ -14,7 +12,7 @@ const HeaderNav = () => {
                 <div className="row">
                     <div className="col-md-4"></div>
                     <div className="col-md-5">
-                        <p className="tag-line"> Life is short make every hair flip count </p>
+                        <p className="tag-line"> Life is short make every hair flip count</p>
                     </div>
                     <div className="col-md-3">
                         <div className="top-bar-side">
@@ -22,11 +20,16 @@ const HeaderNav = () => {
                                 <option value="1">USD $</option>
                                 <option value="2">GMD D</option>
                             </select>
-                            <p onClick={() => {navigate('/track')}}>Track order  <i className="fa-solid fa-truck-fast"></i></p>
+                            <p
+                                onClick={() => {
+                                    navigate("/track");
+                                }}
+                            >
+                                Track order <i className="fa-solid fa-truck-fast"></i>
+                            </p>
                         </div>
                     </div>
                 </div>
-
             </div>
             <div className="Header-nav">
                 <nav className="navbar navbar-expand-lg navbar-dark">
@@ -35,38 +38,70 @@ const HeaderNav = () => {
                         Hair by Tima Blaq
                     </Link>
 
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
                     </button>
+
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
-                                <Link to="/" className="nav-link">Home <span className="sr-only">(current)</span></Link>
+                                <Link to="/" className="nav-link">
+                                    Home <span className="sr-only">(current)</span>
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/shop" className="nav-link">shop</Link>
+                                <Link to="/shop" className="nav-link">
+                                    shop
+                                </Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/about" className="nav-link">About</Link>
+                                <Link to="/about" className="nav-link">
+                                    About
+                                </Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/contact" className="nav-link">Contact</Link>
+                                <Link to="/contact" className="nav-link">
+                                    Contact
+                                </Link>
                             </li>
-
                         </ul>
                         <form className="form-inline my-2 my-lg-0">
-                            <p><i className="fa-solid fa-magnifying-glass"></i> </p>
-                            <p onClick={() => {navigate('/track')}} ><i className="fa-solid fa-truck-fast"></i> </p>
-                            <p onClick={() => {navigate('/cart')}} ><i className="fa-solid fa-cart-shopping"></i> <span className="cart-number">(1)</span>  </p>
+                            <p>
+                                <i className="fa-solid fa-magnifying-glass"></i>{" "}
+                            </p>
+                            <p
+                                onClick={() => {
+                                    navigate("/track");
+                                }}
+                            >
+                                <i className="fa-solid fa-truck-fast"></i>{" "}
+                            </p>
+                            <p
+                                onClick={() => {
+                                    navigate("/cart");
+                                }}
+                            >
+                                <i className="fa-solid fa-cart-shopping"></i>{" "}
+                                <span className="cart-number">(1)</span>{" "}
+                            </p>
                         </form>
                     </div>
                 </nav>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default HeaderNav;
