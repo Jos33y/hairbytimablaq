763cdb314218d20b4bc2307ebb1eb5b0ec7a0f28
Jos33y <josephlagbalu@gmail.com>
2023-05-05 14:18:07 -0500

    major things working fine remain mobile responsiveness and track order page

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("/", function (_, res) {
+app.get("/*", function (_, res) {
     res.sendFile(
         path.join(__dirname, "../client/build/index.html"),
         function (err) {
--- client/src/store/components/format-price.js
+++ client/src/store/components/format-price.js
@@ -3,7 +3,7 @@ import he from 'he';
 const localRate = JSON.parse(localStorage.getItem("rate"));
 
 export const formatPrice = (price) => {
-  return Number(price / localRate.rateAmount).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return Number(price / (localRate ? localRate.rateAmount : 1)).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 export const formatSymbol = () => {
